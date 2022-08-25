ffce3ea33be7135013c002cd780ef768a6bb7563
EC2 Default User <ec2-user@ip-172-31-56-114.us-west-2.compute.internal>
2022-08-25 08:14:36 +0000

    Adding synth

--- lib/pipeline.ts
+++ lib/pipeline.ts
@@ -2,10 +2,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as blueprints from '@aws-quickstart/eks-blueprints';
-import autoscaling = require('aws-cdk-lib/aws-autoscaling');
-import iam = require('aws-cdk-lib/aws-iam');
-import ec2 = require('aws-cdk-lib/aws-ec2');
-import eks = require('aws-cdk-lib/aws-eks');
 
 // import { TeamPlatform, TeamApplication } from '../teams';
 
@@ -19,43 +15,8 @@ export default class PipelineConstruct extends Construct {
     const blueprint = blueprints.EksBlueprint.builder()
     .account(account)
     .region(region)
-    .addOns(new blueprints.ClusterAutoScalerAddOn);
-    // .teams(new TeamPlatform(account), new TeamApplication('burnham',account));
+    .addOns(new blueprints.ClusterAutoScalerAddOn);// .teams(new TeamPlatform(account), new TeamApplication('burnham',account));
   
-    const vpc = new ec2.Vpc(this, 'EKSVpc');  // Create a new VPC for our cluster
-    
-    // IAM role for our EC2 worker nodes
-    const workerRole = new iam.Role(this, 'EKSWorkerRole', {
-      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com')
-    });
-
-    const eksCluster1 = new eks.Cluster(this, 'Training', {
-      defaultCapacity: 0,  // we want to manage capacity our selves
-      version: eks.KubernetesVersion.V1_21,
-    });
-    
-    const eksCluster2 = new eks.Cluster(this, 'Inference', {
-      defaultCapacity: 0,  // we want to manage capacity our selves
-      version: eks.KubernetesVersion.V1_21,
-    });
-
-    const onDemandASG = new autoscaling.AutoScalingGroup(this, 'OnDemandASG', {
-      vpc: vpc,
-      role: workerRole,
-      minCapacity: 1,
-      maxCapacity: 10,
-      instanceType: new ec2.InstanceType('t3.medium'),
-      machineImage: new eks.EksOptimizedImage({
-        kubernetesVersion: '1.21',
-        nodeType: eks.NodeType.STANDARD  // without this, incorrect SSM parameter for AMI is resolved
-      }),
-      updatePolicy: autoscaling.UpdatePolicy.rollingUpdate()
-      });
-
-    eksCluster1.connectAutoScalingGroupCapacity(onDemandASG, {})
-    eksCluster2.connectAutoScalingGroupCapacity(onDemandASG, {});
-    
-    
     const repoUrl = 'https://github.com/aws-samples/eks-blueprints-workloads.git';
 
     const bootstrapRepo : blueprints.ApplicationRepository = {
@@ -102,3 +63,46 @@ export default class PipelineConstruct extends Construct {
       .build(scope, id+'-stack', props);
   }
 }
+
+import autoscaling = require('aws-cdk-lib/aws-autoscaling');
+import iam = require('aws-cdk-lib/aws-iam');
+import ec2 = require('aws-cdk-lib/aws-ec2');
+import eks = require('aws-cdk-lib/aws-eks');
+
+class EKSCluster extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const vpc = new ec2.Vpc(this, 'EKSVpc');  // Create a new VPC for our cluster
+    
+    // IAM role for our EC2 worker nodes
+    const workerRole = new iam.Role(this, 'EKSWorkerRole', {
+      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com')
+    });
+
+    const eksCluster = new eks.Cluster(this, 'Cluster', {
+      vpc: vpc,
+      defaultCapacity: 0,  // we want to manage capacity our selves
+      version: eks.KubernetesVersion.V1_21,
+    });
+
+    const onDemandASG = new autoscaling.AutoScalingGroup(this, 'OnDemandASG', {
+      vpc: vpc,
+      role: workerRole,
+      minCapacity: 1,
+      maxCapacity: 10,
+      instanceType: new ec2.InstanceType('t3.medium'),
+      machineImage: new eks.EksOptimizedImage({
+        kubernetesVersion: '1.21',
+        nodeType: eks.NodeType.STANDARD  // without this, incorrect SSM parameter for AMI is resolved
+      }),
+      updatePolicy: autoscaling.UpdatePolicy.rollingUpdate()
+      });
+
+    eksCluster.connectAutoScalingGroupCapacity(onDemandASG, {});
+  }
+}
+
+const app = new cdk.App();
+new EKSCluster(app, 'MyEKSCluster');
+app.synth();
